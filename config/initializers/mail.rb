7d4c4406a8e575df41ec43d9e77e472c14d34eda
Ubuntu <ubuntu@ip-172-31-53-104.ec2.internal>
2022-05-23 19:09:31 +0000

    set up production end for heroku again 31x

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -6,6 +6,6 @@ ActionMailer::Base.smtp_settings = {
 :user_name => ENV["SENDGRID_USERNAME"],
 :password => ENV["SENDGRID_PASSWORD"],
 :domain => 'heroku.com',
-:enable_starttls_auto => true,
-:openssl_verify_mode => "none"
+:enable_starttls_auto => true
+
 }
--- gp
+++ gp
@@ -1,4 +1,4 @@
 git add -A
-git commit -m "set up production end for heroku again 30x"
+git commit -m "set up production end for heroku again 31x"
 git push origin master
 git push heroku master
