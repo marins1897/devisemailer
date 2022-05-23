04f294625a0abf10626bd2481c60b7d0fce9e0c0
Ubuntu <ubuntu@ip-172-31-53-104.ec2.internal>
2022-05-23 17:49:57 +0000

    set up production end for heroku again 22x

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  Rails.application.routes.default_url_options = { host: 'https://marinarticles.herokuapp.com/'}
+  config.action_mailer.default_url_options = { host: 'http://marinarticles.herokuapp.com/'}
   
 end
